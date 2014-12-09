68cb53d198dc320f2dc1e987d066c0610b10c8a3
aafan <lancer.oxcom@gmail.com>
2014-12-09 10:56:26 +0300

    autoloader hack

--- lib/autoloader.php
+++ lib/autoloader.php
@@ -5,12 +5,21 @@ class CDNVideoLoader
 {
     public static function load($file)
     {
-        $file   = str_replace('\\', '/', $file);
+        $file   = str_replace('\\', DIRECTORY_SEPARATOR, $file);
         $path   = realpath(__DIR__);
         $target = $path . DIRECTORY_SEPARATOR . $file . '.php';
 
         if (is_file($target)) {
             include_once $target;
+        } else {
+            // hack (CentOS + Bitrix => namespace name in $file = \CDNVideo\Settings instead of \CDNVideo\Tools\Settings)
+            // @TODO: remove this!!!
+            $file = explode(DIRECTORY_SEPARATOR, $file);
+            $file = implode(DIRECTORY_SEPARATOR, array($file[0], 'Tools', $file[1]));
+            
+            if (is_file($target)) {
+                include_once $target;
+            }
         }
     }
 
